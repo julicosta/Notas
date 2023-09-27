b3a441b88f9a5e088cfe246e0e10a88ee444035e
julicosta <julianarielcosta@gmail.com>
2023-09-27 16:48:59 -0300

    Agregado app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+// console.log("Hola Mundo")
+let a = 10;
+let b = 20;
+let c = 30;
+let x= a+b;
+let f= "Hola ";
+let h= "Spiderman"
+//let d= 10, g=10, m=10, l=d+g+m;
+const saludo = f+h;
+//console.table({a,b,c,x});
+
+//console.log({a});
+//console.log({b});
+//console.log({c});
+//console.log(x);
+//console.warn(x);
+//console.error(x);
+//console.info(x);
+
+// a=20; // se puede hacer
+// b=20; // se puede hacer
+// c=20; // no se puede hacer
